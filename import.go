d5e62ce0718051f06fa5702a86d0271138e7ae1e
Chris Reeves <chris@thisissoon.com>
2015-07-06 14:22:00 +0100

    Import stub

---
+++ import.go
@@ -0,0 +1,12 @@
+// Imports a txt file that will contain line seperated pool of
+// server naames.
+
+package sir
+
+import "gopkg.in/redis.v3"
+
+// Imports a txt file, reading each line and saving each line
+// into a redis set
+func Import(p string, r *redis.Client) {
+
+}
