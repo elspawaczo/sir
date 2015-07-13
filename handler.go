b2e1d3ade15bcaf56698c468cb25f8375087dcb1
Chris Reeves <chris@thisissoon.com>
2015-07-13 12:26:21 +0100

    Application http handler type

---
+++ handler.go
@@ -0,0 +1,39 @@
+// Application HTTP Handler
+
+package sir
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/zenazn/goji/web"
+)
+
+// Base HTTP Handler Type emending our Applicaton Conext Type
+type ApplicationHandler struct {
+	*ApplicationContext
+	h func(*ApplicationContext, web.C, http.ResponseWriter, *http.Request) (int, error)
+}
+
+// Satisfy http.Handler, which Goji's web.Handler extends.
+func (a ApplicationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.ServeHTTPC(web.C{}, w, r)
+}
+
+// Custom Serve HTTP handler which provides our application context to our
+// route handler functions
+func (h ApplicationHandler) ServeHTTPC(c web.C, w http.ResponseWriter, r *http.Request) {
+	status, err := h.h(h.ApplicationContext, c, w, r)
+	// Handler errors
+	if err != nil {
+		log.Printf("HTTP %d: %q", status, err)
+		switch status {
+		case http.StatusNotFound:
+			http.NotFound(w, r)
+		case http.StatusInternalServerError:
+			http.Error(w, http.StatusText(status), status)
+		default:
+			http.Error(w, http.StatusText(status), status)
+		}
+	}
+}
